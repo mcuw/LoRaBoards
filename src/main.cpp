11fc92f75c6b02025f5f0f490d025f23f6f0f0e9
mcuw <112967559+mcuw@users.noreply.github.com>
2026-04-02 12:09:46 +0200

    Create Arduino library: MCUW ESP32 LoRa boards

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,4 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <WiFi.h>
-#include <SPI.h>
-#include <SD.h>
 #include "LgLoraBoard.h"
 
 LgLoraBoard board;
@@ -10,9 +6,10 @@ LgLoraBoard board;
 void setup() {
   Serial.begin(115200);
   Serial.println("LoRa Application");
+
   board.setupRadioBoard();
 }
 
 void loop() {
-  delay(10000);
+  delay(1000);
 }
