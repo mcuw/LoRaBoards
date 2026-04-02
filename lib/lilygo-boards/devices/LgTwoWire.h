730f5478d551db64a9dd70fcffb57feed7189d2a
mcuw <112967559+mcuw@users.noreply.github.com>
2026-04-02 09:39:14 +0200

    doc: LgTwoWire

--- lib/lilygo-boards/devices/LgTwoWire.h
+++ lib/lilygo-boards/devices/LgTwoWire.h
@@ -7,6 +7,6 @@ void scanDevices(TwoWire *w);
 
 extern uint8_t  bme280_address;     // It might be 0x76
 extern uint8_t  mag_address;        // QMC6310U=0x1C QMC6310N=0x3C
-extern uint8_t  display_address;    // It might be 0x3D
+extern uint8_t  display_address;    // It might be 0x3C or 0x3D
 
 #endif /* LG_TWOWIRE_H */
